99d773a4691fc4d30180427c3fd9e3daef086f8b
Andrey-Pavlov-A <andreyloonohod4@gmailcom>
2021-06-05 18:08:47 +0300

    scrolling added

--- src/containers/StoriesConytainer.jsx
+++ src/containers/StoriesConytainer.jsx
@@ -1,9 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import { getStoriesId, getStory } from '../api/api'
 import Story from '../components/Story'
+import { useInfiniteScroll } from '../hooks/useInfiniteScroll'
 import { StoriesContainerWrapper } from '../utils/StoriesConytainerStyles'
 
 const StoriesConytainer = () => {
+
+  const {count} = useInfiniteScroll()
+
 const [storyIds, setStoryIds] = useState([])
 
   useEffect(() => {
@@ -15,7 +19,7 @@ const [storyIds, setStoryIds] = useState([])
   return (
     <StoriesContainerWrapper>
       <h1>Hacker News Page</h1>
-      {storyIds.map((story, index) => <Story storyId={story} index={index}/>)}
+      {storyIds.slice(0, count).map((story, index) => <Story storyId={story} index={index}/>)}
     </StoriesContainerWrapper>
   );
 }
